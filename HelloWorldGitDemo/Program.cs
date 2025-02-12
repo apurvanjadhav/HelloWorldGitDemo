dd9705319f8190302e94a6418ec91088ee3a4d0b
apunaray <apunaray@LIN-5CG134BVBV>
2025-02-12 12:24:05 +0530

    Add project files.

---
+++ HelloWorldGitDemo/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace HelloWorldGitDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("We are learning GIT!");
+        }
+    }
+}
